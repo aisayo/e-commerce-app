e0461e3b1bfe386cf7e57a24d678f4a79b48dd0d
Aysan Isayo <aisayo@icloud.com>
2018-12-06 15:22:53 -0800

    created HomePage component and added to Index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HomePage from '../src/HomePage'
 
-
-const App = () => { return <div>testing</div> }
+const App = () => { return <HomePage /> }
 
 ReactDOM.render(
     <App />,
